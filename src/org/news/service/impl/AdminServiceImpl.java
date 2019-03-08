3dcc013ef71f40fd1fb7e671c1baa2fa7b9814de
fushenruomeng <942379413@qq.com>
2019-03-08 09:55:04 +0800

    tgs

--- src/org/news/service/impl/AdminServiceImpl.java
+++ src/org/news/service/impl/AdminServiceImpl.java
@@ -5,6 +5,6 @@ import org.news.service.AdminService;
 public class AdminServiceImpl implements AdminService {
     @Override
     public void xm2() {
-        String tgs="田贵燊";
+        String tgs="田贵燊1";
     }
 }
